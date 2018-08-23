4c3856706a1bd2cc4e4a5058a5206fb4fdb22d29
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-23 19:41:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,6 +50,9 @@ class Trip{
     }
     store.trips.push(this);
   }
+  setDriver(driver){
+    this.driverId = driver.id;
+  }
   driver(){
     return store.drivers.find(function(driver){
       return driver.id === this.driverId;
