9dfaa0a5fdda708883aa644f2afd6bd63f24ab30
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-23 19:41:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -42,8 +42,12 @@ class Passenger{
 class Trip{
   constructor(driver,passenger){
     this.id = ++tripId;
+    if (driver){
     this.driverId = driver.id;
+    }
+    if (passenger){
     this.passengerId = passenger.id;
+  }
     store.trips.push(this);
   }
   driver(){
