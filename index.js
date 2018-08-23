8290f9dc99346a301596a666fd77be46e7b3c3db
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-23 19:39:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ class Driver {
   }
   passengers(){
     return store.passengers.filter(pass =>{
-      return pass.trip.id === this.trip.id;
+      return pass.trip === this.trip;
     })
   }
 }
